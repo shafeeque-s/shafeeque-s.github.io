42d7be1c038a7accfd063baabd4db80ea9841e10
shefik <shefik000@gmail.com>
2016-11-03 20:55:51 +0530

    new layout updation

---
+++ index.php
@@ -0,0 +1,50 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="assets/css/mystyle.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+    <div class="container top-container">
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo"><img src="assets/images/logo.png" class="responsive-img"></a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">About & More</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Resources</a></li>
+          </ul>
+          <ul class="side-nav" id="mobile-demo">
+            <li><a href="#">About & More</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Resources</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <!-- Shafeeque Sheriff — A product designer from Prague, Czech Republic, who focuses on interactive experiences & mobile apps, currently co-leading a design team at STRV. -->
+
+      <div class="row">
+        <div class="offset-m4 col m8">
+          <p class="info-main">Shafeeque S — A frontend developer from Bengaluru, India,  love to exploring new languages, cutting edge technologies & implementing them to the projects. Currently co-leading a front end development team at <a href="#">SYNCLOVIS</a></p>
+          <p>Sharing most of the design work on Dribbble and Behance, random thoughts on Twitter, every day moments on Instagram and articles on Medium. Full resumé is available on LinkedIn.</p>
+        </div>
+      </div>
+    </div>
+
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <!-- Compiled and minified JavaScript -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+      <script src="assets/js/myscript.js"></script>
+    </body>
+  </html>
